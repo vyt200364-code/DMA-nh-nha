8862c2d0b8455df5ced1be85d6cd9790a20a1fa3
vyt200364-code <vyt200364@gmail.com>
2026-07-29 10:45:40 +0700

    questions.js

---
+++ questions.js
@@ -0,0 +1,63 @@
+const questions = [
+
+{
+    question: "2 + 2 = ?",
+
+    multi: false,
+
+    options: [
+
+        {
+            text: "3",
+            correct: false
+        },
+
+        {
+            text: "4",
+            correct: true
+        },
+
+        {
+            text: "5",
+            correct: false
+        },
+
+        {
+            text: "6",
+            correct: false
+        }
+
+    ]
+},
+
+{
+    question: "Choose all colors",
+
+    multi: true,
+
+    options: [
+
+        {
+            text: "Red",
+            correct: true
+        },
+
+        {
+            text: "Blue",
+            correct: true
+        },
+
+        {
+            text: "Dog",
+            correct: false
+        },
+
+        {
+            text: "Cat",
+            correct: false
+        }
+
+    ]
+}
+
+];
